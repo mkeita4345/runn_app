b2cf9bdd9918bb928eae4d41bbb4f8e53cf4a0dc
mkeita4345 <153411666+mkeita4345@users.noreply.github.com>
2025-04-27 03:37:22 -0500

    Ran initial migrations and set up database and added homepage

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,5 @@
 # users/urls.py
+# users/urls.py
 from django.urls import path
 from . import views
 
@@ -6,4 +7,6 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('register/', views.register_view, name='register'),
+    path('home/', views.home_view, name='home'),  # <-- ADD THIS
 ]
+
--- users/views.py
+++ users/views.py
@@ -2,7 +2,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, authenticate, logout
-
+from django.contrib.auth.decorators import login_required
 def register_view(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
@@ -28,4 +28,6 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return redirect('login')
-
+@login_required
+def home_view(request):
+    return render(request, 'users/home.html')
